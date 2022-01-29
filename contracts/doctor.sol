e9dd9e10857053c9f2cad35b499a11fd4666be89
Mohsin Chougale <mohsin.chougale@gmail.com>
2022-01-29 11:52:16 +0530

    Edited smart contracts for patient and doctor
    Created struct for patient and doctor. Also wrote a function to add patient and doctor details

--- contracts/doctor.sol
+++ contracts/doctor.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
+
+contract Doctors {
+    int256 public id = 0;
+    mapping(int256 => Doctor) public doctor;
+
+    struct Doctor {
+        string name;
+        string phoneNumber;
+        string specialization;
+        address walletAddress;
+        string publicKey;
+    }
+
+    function addDoctor(
+        string memory name,
+        string memory phoneNumber,
+        string memory specialization,
+        string memory publicKey
+    ) public {
+        id += 1;
+        doctor[id] = Doctor(
+            name,
+            phoneNumber,
+            specialization,
+            msg.sender,
+            publicKey
+        );
+    }
+}
--- contracts/patient.sol
+++ contracts/patient.sol
@@ -1,1 +1,43 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
+
+contract Patients {
+    int256 public id = 0;
+    mapping(int256 => Patient) public patient;
+
+    struct Patient {
+        string name;
+        uint256 age;
+        uint256 height;
+        uint256 weight;
+        string gender;
+        string phoneNumber;
+        string homeAddress;
+        address walletAddress;
+        string publicKey;
+    }
+
+    function addPatient(
+        string memory name,
+        uint256 age,
+        uint256 height,
+        uint256 weight,
+        string memory gender,
+        string memory phoneNumber,
+        string memory homeAddress,
+        string memory publicKey
+    ) public {
+        id += 1;
+        patient[id] = Patient(
+            name,
+            age,
+            height,
+            weight,
+            gender,
+            phoneNumber,
+            homeAddress,
+            msg.sender,
+            publicKey
+        );
+    }
+}
